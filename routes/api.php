56f675b2f0702dc8af60640e3a997206c486f91a
elizabetabeta <elizabeta.milicevic7@gmail.com>
2022-03-05 14:40:22 +0100

    svasta nesta

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
@@ -18,6 +19,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('auth')->group(function(){
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::get('user', [AuthController::class, 'user']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::post('register', [AuthController::class, 'register']);
 });
 
 //REST API // CRUD
