51edadcfaa8c1a51bf4b02240c897e524db95654
elizabetabeta <elizabeta.milicevic7@gmail.com>
2022-03-05 14:55:40 +0100

    Svasta nesta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Administracija korisnika</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,31 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table">
+                        <tr>
+                            <th>ID</th>
+                            <th>Ime korisnika</th>
+                            <th>E-mail korisnika</th>
+                            <th>Uloga korisnika</th>
+                            <th>Datum i vrijeme registracije</th>
+                            <th>Akcije</th>
+                        </tr>
+
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->role }}</td>
+                            <td>{{ $user->created_at }}</td>
+                            <td>
+                                <a href="{{ route("users.delete", $user->id) }}" class = "btn btn-danger">Obriši</a>
+                            </td>
+                        </tr>
+                        @endforeach
+
+                    </table>
+
                 </div>
             </div>
         </div>
