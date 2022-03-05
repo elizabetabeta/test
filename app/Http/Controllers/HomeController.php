602cf40da49da06f47999d12ecb1dda8a7c8f1f9
elizabetabeta <elizabeta.milicevic7@gmail.com>
2022-03-05 14:55:23 +0100

    Svasta nesta

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = DB::table('users')->get();
+
+        return view('home', ["users"=>$user]);
+    }
+
+    public function delete($id)
+    {
+        DB::table('users')->where("id", $id)->delete();
+        return redirect('/login');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Device::class);
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
