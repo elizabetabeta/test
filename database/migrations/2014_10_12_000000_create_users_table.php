60471be6fe98b5e5199dc92277497a08b8880977
elizabetabeta <elizabeta.milicevic7@gmail.com>
2022-03-08 14:32:36 +0100

    svega i svacega

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum("role_user", ['Admin','Moderator','User'])->default('User');
+            $table->enum("role", ['Admin','Moderator','User'])->default('User');
             $table->rememberToken();
             $table->timestamps();
         });
